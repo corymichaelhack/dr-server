45421f4d704f1942ea0c9c8dc9cb9b44c048861d
Cory Hack <cory.hack@gmail.com>
2019-12-03 16:33:39 -0500

    Remove multer fileFilter file and references

--- middleware/imageFilters.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true)
-};
-

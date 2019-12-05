bdb78a75f27e98d748a6cd9217a04daacf50ef22
Jesse Smith <jessesmith@Jesses-MBP.hsd1.in.comcast.net>
2019-12-05 16:05:11 -0500

    implemented auth0 for user authentication

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config(); //to help hide our token signatures
 let express = require('express');
 let app = express();
-
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cors = require('cors');
 //CONTROLLERS
 let artist = require('./controllers/artist-controller');
 let skill = require('./controllers/skill-controller');
 let feedback = require('./controllers/feedback-controller');
+let config = require('./config');
 
 let sequelize = require('./db');
 sequelize.sync(); //tip pass in {force:true} for resetting all tables
@@ -13,6 +17,10 @@ sequelize.sync(); //tip pass in {force:true} for resetting all tables
 //APP USE
 app.use(express.json())// to use the req.body middleware
 app.use(require('./middleware/headers'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(cors());
 
 // app.use("/test", function(req, res){
 //     res.send("data from server")
@@ -26,11 +34,19 @@ app.use(require('./middleware/validate-session'));
 app.use('/skill', skill); //call skill routes
 // app.use('/', feedback); //call feedback routes
 
+if (process.env.NODE_ENV !== 'dev') {
+    app.use('/', express.static(path.join(__dirname, './dist')));
+  }
+  
+require('./api')(app, config);
 
-
-
-
-
-app.listen(3000, () => {
+if (process.env.NODE_ENV !== 'dev') {
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, '/dist/index.html'));
+    });
+  }
+  
+  
+app.listen(process.env.PORT, () => {
     console.log(`Hello from ${process.env.PORT}.`);
 })
